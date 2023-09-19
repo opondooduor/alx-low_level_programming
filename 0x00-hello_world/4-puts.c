77ebb720b6bb2c8cdc7fa125463a96513525bf8f
opondooduor <austineoduor289@gmail.com>
2023-09-19 07:30:58 -0700

    main.h

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programn that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts ("\" programing is like buiding a multilingual puzzle");
+	returns (0);
+}
